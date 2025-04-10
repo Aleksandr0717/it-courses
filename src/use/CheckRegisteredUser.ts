03aa6cd37a98963b55fc0227f72d887b494ef01c
Aleksandr0717 <kochetkov_a02@mail.ru>
2025-04-10 20:02:20 +0300

    Доработка

--- src/use/CheckRegisteredUser.ts
+++ src/use/CheckRegisteredUser.ts
@@ -1,11 +1,11 @@
 import { computed, onMounted, ref, watch } from "vue";
-import { useUserStore } from "@/stores/userStore.js";
+import { useUserStore } from "@/stores/UserStore.js";
 
 export function useCheckRegisteredUser() {
   const userStore = useUserStore();
   const currentUser = computed(() => userStore.currentUser)
   const unRegisteredUser = ref(true);
-  
+
   onMounted(() => {
     const localStorageUser = localStorage.getItem("currentUser");
     const sessionStorageUser = sessionStorage.getItem("currentUser")
@@ -21,14 +21,14 @@ export function useCheckRegisteredUser() {
       userStore.INIT_CURRENT_USER(parseInt(JSON.parse(sessionStorageUser)));
     }
   })
-  
+
   watch(currentUser, (newValue: object) => {
     if (Object.keys(newValue)?.length) unRegisteredUser.value = false;
     else {
       unRegisteredUser.value = true;
     }
   }, {immediate: true, deep: true});
-  
+
   return {
     unRegisteredUser,
   }
